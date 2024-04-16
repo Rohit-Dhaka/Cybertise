99ee696d56cecc17172a04fcc1d4ab83dc47d0a8
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-04-16 12:50:00 +0530

    add navbar link

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,7 @@ function mobileMenu() {
 let mybutton = document.getElementById("myBtn");
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
